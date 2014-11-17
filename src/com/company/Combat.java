a6416a2d38261874c8b39a992445fe4d7dce332c
Daniel Eineving <daniel@eineving.se>
2014-11-17 18:14:28 +0100

    Implemented result calculations

--- src/com/company/Combat.java
+++ src/com/company/Combat.java
@@ -1,25 +1,95 @@
 package com.company;
 
+import java.util.ArrayList;
+
 /**
- * Created by Test on 2014-11-16.
+ * Created by Daniel on 2014-11-16.
  */
 public class Combat {
-    private int attackers, defenders;
-    public Combat(int attackers, int defenders){
-        this.attackers=attackers;
-        this.defenders=defenders;
+    private int nbrOfAttackers, nbrOfDefenders;
+
+    public Combat(int attackers, int defenders) {
+        this.nbrOfAttackers = attackers;
+        this.nbrOfDefenders = defenders;
     }
 
-    public void rollOnce(){
-
-    }
-
-    public void simulateOutcome(){
+    public void rollOnce() {
 
     }
 
-    public boolean attackersWin(){
+    public void simulateOutcome() {
+
+    }
+
+    public boolean attackersWin() {
         //TODO fix
         return false;
+    }
+
+    private void executeResult(ArrayList<Die> attackers, ArrayList<Die> defenders) {
+        int bigAttackValue = -1;
+        int secondAttackValue = -1;
+        int bigDefendValue = -1;
+        int secondDefendValue = -1;
+
+        if (attackers.size() > 3 || defenders.size() > 2) {
+            throw new IllegalArgumentException("To many dice");
+        }
+
+        //region SettingValues
+        //If the attacker has two or more dice
+        if (attackers.size() > 1) {
+            for (Die aDie : attackers) {
+                if (aDie.getLastValue() > bigAttackValue) {
+                    secondAttackValue = bigAttackValue;
+                    bigAttackValue = aDie.getLastValue();
+                } else if (aDie.getLastValue() > secondAttackValue) {
+                    secondAttackValue = aDie.getLastValue();
+                }
+            }
+        }
+
+        //If the attacker has one die
+        else {
+            bigAttackValue = attackers.get(0).getLastValue();
+        }
+
+        //If the attacker has two or more dice
+        if (defenders.size() > 1) {
+            bigDefendValue = -1;
+            secondDefendValue = -1;
+
+            for (Die dDie : defenders) {
+                if (dDie.getLastValue() > bigDefendValue) {
+                    secondDefendValue = bigDefendValue;
+                    bigDefendValue = dDie.getLastValue();
+                } else if (dDie.getLastValue() > secondDefendValue) {
+                    secondDefendValue = dDie.getLastValue();
+                }
+            }
+        }
+
+        //If the attacker has one die
+        else {
+            bigDefendValue = defenders.get(0).getLastValue();
+        }
+        //endregion
+
+
+        //If the attacker is greater than the defender
+        if (bigAttackValue > bigDefendValue) {
+            nbrOfDefenders--;
+        } else {
+            nbrOfAttackers--;
+        }
+
+        if (attackers.size() > 1 && defenders.size() > 1) {
+            if (secondAttackValue > secondDefendValue) {
+                nbrOfDefenders--;
+            } else {
+                nbrOfAttackers--;
+            }
+        }
+
     }
 }
--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -4,7 +4,28 @@ package com.company;
  * Created by Daniel Eineving on 2014-11-16.
  */
 public class Die {
-    public static int rollDie(){
-        return (int)(Math.random()*6)+1;
+    private int lastValue = -1;
+    private int sides;
+
+    /**
+     * Create a new die
+     * @param sides nbr of sides on the die
+     */
+    public Die(int sides) {
+        this.sides = sides;
+    }
+
+    public int rollDie() {
+        lastValue = (int) (Math.random() * sides) + 1;
+        return lastValue;
+    }
+
+    /**
+     * Get last value of the die
+     *
+     * @return the last value, -1 if the die have never been rolled
+     */
+    public int getLastValue() {
+        return lastValue;
     }
 }
